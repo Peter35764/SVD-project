b4903f24bff3bdfe9cf292851c9fdbc1a8217830
Dist <vlad244244@gmail.com>
2025-03-17 23:06:18 +0300

    class for storing data in a table

--- src/gui.cpp
+++ src/gui.cpp
@@ -4,19 +4,25 @@
 #include <fstream>
 #include <sstream>
 
+/*
+ * 1. Потыкать таблицу(2-й патч)
+ * 2. Заменить Qtable на CustomTable
+ * 3. Научиться трактовать паршенное значение
+ */
+
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent) {
     setWindowTitle("SVD Test Application");
     resize(400, 200);
 
     QWidget *centralWidget = new QWidget(this);
     QVBoxLayout *layout = new QVBoxLayout(centralWidget);
-    
+
     algorithmSelector = new QComboBox(this);
     algorithmSelector->addItem("Jacobi SVD", QVariant("jacobi"));
     algorithmSelector->addItem("DQDS SVD", QVariant("dqds"));
-    
+
     startButton = new QPushButton("Run SVD Tests", this);
-    
+
     layout->addWidget(algorithmSelector);
     layout->addWidget(startButton);
     layout->addStretch();
@@ -35,7 +41,7 @@ MainWindow::~MainWindow() {
 void MainWindow::runTests() {
     startButton->setEnabled(false);
     algorithmSelector->setEnabled(false);
-    
+
     QString selectedAlgo = algorithmSelector->currentData().toString();
     currentResultFile = selectedAlgo.toStdString() + "_test_table.txt";
 
@@ -43,17 +49,17 @@ void MainWindow::runTests() {
     std::vector<double> ratios = {1.01, 1.2, 2, 5, 10, 50};
     std::vector<std::pair<int,int>> sizes = {{3,3}, {5,5}, {10,10}, {20,20}, {50,50}, {100,100}};
     int n = 20;
-
+/*
     // Вызов svd_test_func в зависимости от выбранного алгоритма
     if (selectedAlgo == "jacobi") {
         svd_test_func<double, SVDGenerator, Eigen::JacobiSVD>(currentResultFile, ratios, sizes, n);
     } else if (selectedAlgo == "dqds") {
         svd_test_func<double, SVDGenerator, DQDS_SVD>(currentResultFile, ratios, sizes, n);
     }
-
+*/
     createResultWindow();
     populateTable(currentResultFile);
-    
+
     startButton->setEnabled(true);
     algorithmSelector->setEnabled(true);
 }
@@ -67,22 +73,22 @@ void MainWindow::createResultWindow() {
     resultWindow->resize(800, 600);
 
     QVBoxLayout *layout = new QVBoxLayout(resultWindow);
-    
+
     resultTable = new QTableWidget(resultWindow);
     QPushButton *saveButton = new QPushButton("Save Table", resultWindow);
-    
+
     layout->addWidget(resultTable);
     layout->addWidget(saveButton);
-    
+
     connect(saveButton, &QPushButton::clicked, this, &MainWindow::saveTable);
-    
+
     resultWindow->setLayout(layout);
     resultWindow->show();
 }
 
 void MainWindow::populateTable(const std::string& filename) {
     tableData.clear();
-    
+
     std::ifstream file(filename);
     if (!file) {
         QMessageBox::warning(this, "Error", "Could not open result file!");
@@ -94,7 +100,7 @@ void MainWindow::populateTable(const std::string& filename) {
         std::vector<std::string> row;
         std::stringstream ss(line);
         std::string cell;
-        
+
         while (ss >> cell) {
             row.push_back(cell);
         }
@@ -106,13 +112,13 @@ void MainWindow::populateTable(const std::string& filename) {
 
     resultTable->setRowCount(tableData.size() - 1);
     resultTable->setColumnCount(tableData[0].size());
-    
+
     QStringList headers;
     for (const auto& header : tableData[0]) {
         headers << QString::fromStdString(header);
     }
     resultTable->setHorizontalHeaderLabels(headers);
-    
+
     for (size_t i = 1; i < tableData.size(); ++i) {
         for (size_t j = 0; j < tableData[i].size(); ++j) {
             resultTable->setItem(i-1, j, new QTableWidgetItem(
@@ -122,9 +128,9 @@ void MainWindow::populateTable(const std::string& filename) {
 }
 
 void MainWindow::saveTable() {
-    QString fileName = QFileDialog::getSaveFileName(this, 
+    QString fileName = QFileDialog::getSaveFileName(this,
         "Save Table", "", "Text Files (*.txt);;All Files (*)");
-    
+
     if (fileName.isEmpty()) return;
 
     std::ofstream file(fileName.toStdString());
@@ -141,6 +147,6 @@ void MainWindow::saveTable() {
         file << "\n";
     }
     file.close();
-    
+
     QMessageBox::information(this, "Success", "Table saved successfully!");
 }
--- src/gui.h
+++ src/gui.h
@@ -6,8 +6,10 @@
 #include <QtWidgets/QTableWidget>
 #include <QtWidgets/QVBoxLayout>
 #include <QtWidgets/QComboBox>
+#include <QtWidgets/QScrollArea>
+#include <QtWidgets/QProgressBar>
 #include <vector>
-#include "testing.h"  // Подключаем новый заголовок
+#include "testing.h"
 
 class MainWindow : public QMainWindow {
     Q_OBJECT
@@ -15,6 +17,48 @@ class MainWindow : public QMainWindow {
 public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow() override;
+
+    using pair_= std::pair<int,int>;
+    struct tableValue{
+        enum stored_value{double_, pair__, string_};
+        tableValue(): double_decimal(0), pair_of_int({0,0}), sv(string_){};
+        tableValue(double tmp): double_decimal(tmp), sv(double_){};
+        tableValue(pair_ tmp): pair_of_int(tmp), sv(pair__){};
+        double double_decimal;
+        pair_ pair_of_int;
+        stored_value sv;
+
+    };
+
+    // Кастомный класс QTableWidgetItem
+    class CustomTableWidgetItem : public QTableWidgetItem {
+
+    public:
+        // Конструкторы
+        CustomTableWidgetItem(double tmp) : QTableWidgetItem(), value(tmp){};
+        CustomTableWidgetItem(pair_ tmp) : QTableWidgetItem(), value(tmp){}
+        CustomTableWidgetItem(const QString &text) : QTableWidgetItem(text), value(){}
+
+        // Методы для установки и получения значения
+        void setValue(const tableValue& val) {
+            value = val;
+
+        }
+
+        tableValue getValue() const {
+            return value;
+        }
+
+        // Переопределяем метод clone, чтобы корректно копировать объект
+        CustomTableWidgetItem* clone() const override {
+            CustomTableWidgetItem* item = new CustomTableWidgetItem(*this);
+            item->value = this->value;
+            return item;
+        }
+
+    private:
+        tableValue value;
+    };
 
 private slots:
     void runTests();
@@ -25,11 +69,14 @@ private:
     QComboBox *algorithmSelector;
     QWidget *resultWindow;
     QTableWidget *resultTable;
+    QScrollArea *tableScrollArea;
+	QProgressBar *progressBar;
     std::vector<std::vector<std::string>> tableData;
-    std::string currentResultFile;  // Для хранения имени файла результатов
+    std::string currentResultFile;  
 
-    void createResultWindow();
+    void setupUI();
     void populateTable(const std::string& filename);
+    void createResultWindow();
 };
 
 #endif // GUI_H
