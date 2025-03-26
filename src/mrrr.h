0028639c50dda2940ea7f3e42c6d69fb2a140328
Dist <vlad244244@gmail.com>
2025-03-26 11:33:02 +0300

    feat: update algorithm for framework compatibility

--- src/mrrr.h
+++ src/mrrr.h
@@ -1,30 +1,63 @@
 #ifndef MRRR_H
 #define MRRR_H
 
+#include <cmath>
+#include <cstddef>
+#include <iostream>
+#include <cstdint>
+#include <lapacke.h>
 #include <Eigen/Core>
+#include <Eigen/SVD>
+#include <Eigen/Eigenvalues>
+#include <ostream>
+#include <tuple>
+#include <vector>
+#include <stdexcept>
+#include <algorithm>
+#include <string>
 
-template<typename T>
-class MRRR_SVD{
-	private:
-		Eigen::MatrixXd U;
-		Eigen::MatrixXd S;
-		Eigen::MatrixXd V;
-	void Set_U(const Eigen::MatrixXd& A);
-	void Set_V(const Eigen::MatrixXd& A);
-	void Set_S(const Eigen::MatrixXd& A);
+// Предварительное объявление класса-шаблона MRRR_SVD
+template<typename _MatrixType>
+class MRRR_SVD;
 
-	protected:
-	MRRR_SVD<T> compute_bsvd(const Eigen::MatrixXd& matrix);
+// Специализация traits для MRRR_SVD в пространстве Eigen::internal
+namespace Eigen {
+namespace internal {
+    template<typename _MatrixType>
+    struct traits<MRRR_SVD<_MatrixType>> : public traits<_MatrixType> {
+        typedef _MatrixType MatrixType;
+    };
+}
+}
 
-	public:
-	MRRR_SVD();
-	MRRR_SVD(const Eigen::MatrixXd& A);
+// Объявление класса-шаблона MRRR_SVD с полями и методами
+template<typename _MatrixType>
+class MRRR_SVD : public Eigen::SVDBase<MRRR_SVD<_MatrixType>>
+{
+public:
+    // Конструктор: принимает исходную матрицу и опции вычисления
+    MRRR_SVD(const _MatrixType &matrix, unsigned int computationOptions);
+    
+    // Метод для вычисления разложения TGK
+    MRRR_SVD<_MatrixType>& compute_tgk(const _MatrixType& matrix);
 
-	Eigen::MatrixXd matrixV();
-	Eigen::MatrixXd matrixU();
-	Eigen::MatrixXd singularValues();
+    // Деструктор по необходимости (если потребуется)
+    ~MRRR_SVD() = default;
+
+    // Геттеры для доступа к результатам
+    const Eigen::MatrixXd& matrixU() const { return m_matrixU; }
+    const Eigen::MatrixXd& matrixV() const { return m_matrixV; }
+    const Eigen::VectorXd& singularValues() const { return m_singularValues; }
+
+private:
+    Eigen::MatrixXd m_matrixU;         // Матрица U из SVD
+    Eigen::MatrixXd m_matrixV;         // Матрица V из SVD
+    Eigen::VectorXd m_singularValues;  // Вектор сингулярных значений
+    bool m_isInitialized = false;      // Флаг инициализации
+    bool m_computeFullU = false;       // Флаг вычисления полной матрицы U
+    bool m_computeFullV = false;       // Флаг вычисления полной матрицы V
 };
 
-#include <mrrr.hpp>
+#include "mrrr.hpp" // Подключаем реализацию
 
 #endif // MRRR_H
