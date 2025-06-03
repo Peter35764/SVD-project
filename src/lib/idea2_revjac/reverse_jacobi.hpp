60752e03ad668f19121425e53d751262d7376719
SWYARE <108790835+SWYARE@users.noreply.github.com>
2025-06-03 20:42:54 +0300

    dev: idea2: partly working revJac
    but you need to faith it

--- src/lib/idea2_revjac/reverse_jacobi.hpp
+++ src/lib/idea2_revjac/reverse_jacobi.hpp
@@ -10,7 +10,7 @@
 
 namespace SVD_Project {
 
-const size_t MAX_ITERATIONS = 1000;
+const size_t MAX_ITERATIONS = 400;
 const double TOLERANCE = 1e-10;
 
 template <typename _MatrixType>
@@ -33,29 +33,60 @@ RevJac_SVD<_MatrixType>& RevJac_SVD<_MatrixType>::Compute(
   // Output divergence before iterating (debug)
   Scalar divergence = (currentApproximation - initial).norm();
   if (m_divOstream) {
-    *m_divOstream << "Divergence: " << std::to_string(divergence) << "\n";
+       *m_divOstream << "Divergence: " << std::to_string(divergence) << "\n";
   }
 
+  // This vector represents the order in which the algorithm traverses elements
+  // of reconstructed matrix.
+  // max count of this vector is MAX_ITERATIONS * n(n-1)/2
+  // with each iteration the size increases by n(n-1)/2 elements
+  // its working but there are no explainations =)
+  std::vector<std::pair<std::pair<Index, Index>, Scalar>> traversalOrder;
+  traversalOrder.reserve(MAX_ITERATIONS * initial.rows() * (initial.cols() - 1) / 2);
 
+
+  // Populate the vector with current per-element-divergence (^A_ij - A_ij)
+  // and their corresponding indices
   for (size_t iter = 0; iter < MAX_ITERATIONS; ++iter) {
-    for (Index i = 0; i < initial.rows(); ++i) {
-        for (Index j = 0; j < initial.cols(); ++j) {
+          for (Index i = 0; i < initial.rows(); ++i) {
+              for (Index j = i + 1; j < initial.cols(); ++j) {
+                  traversalOrder.push_back(
+                      {{i, j}, std::abs(currentApproximation(i, j) - initial(i, j))});
+              }
+          }
+
+    // Sort the vector from biggest divergence to smallest
+    std::sort(traversalOrder.begin(), traversalOrder.end(), [](auto a, auto b) {
+        return a.second > b.second;
+    });
+
+    // Perform the main loop
+    for (const auto& [indices, _] : traversalOrder) {
+        Index i = indices.first;
+        Index j = indices.second;
             // Make left and right rotation
             for (const auto& rotType : {RotationType::Left, RotationType::Right}) {
                 Scalar angle = 0.0;
                 Scalar A = 0.0;
                 Scalar B = 0.0;
 
-                // Consider Jacobi rotation on plane (i, j)
+                // Consider Jacobi rotation on rows or columns (i, j)
+                // rotation type accordingly
                 // and find optimal angle to minimize divergence
-                for (const auto& t_I : {i, j})
-                    for (Index t_J = 0; t_J < initial.cols(); ++t_J)
-                        A += currentApproximation(t_I, t_J) * initial(t_I, t_J);
+
+                // Pay attention to this implementation: an algorithm  doesn't find
+                // global minimum of divergence
+                // in a fact, it's looking for a stationary point
+                // regardless of the type of point: local maximum, global minimum and etc.
+
 
                 // Formula for finding stationary points
-                // look at ReverseJacobi in ideas.tex
-
+                // look at ReverseJacobi in ideas.tex page 18-19
                 if (rotType == RotationType::Left) {
+                    for (const auto& t_I : {i, j})
+                        for (Index t_J = 0; t_J < initial.cols(); ++t_J)
+                            A += currentApproximation(t_I, t_J) * initial(t_I, t_J);
+
                     for (Index t_j = 0; t_j < initial.cols(); ++t_j) {
                         B += currentApproximation(i, t_j) * initial(j, t_j);
                     }
@@ -71,6 +102,10 @@ RevJac_SVD<_MatrixType>& RevJac_SVD<_MatrixType>::Compute(
                     currentApproximation.applyOnTheLeft(i, j, rotation.adjoint());
                 }
                 else {
+                    for (const auto& t_I : {i, j})
+                        for (Index t_J = 0; t_J < initial.cols(); ++t_J)
+                            A += currentApproximation(t_J, t_I) * initial(t_J, t_I);
+
                     for (Index t_i = 0; t_i < initial.rows(); ++t_i) {
                         B += currentApproximation(t_i, i) * initial(t_i, j);
                     }
@@ -86,15 +121,14 @@ RevJac_SVD<_MatrixType>& RevJac_SVD<_MatrixType>::Compute(
                     this->m_matrixV.adjointInPlace();
                     currentApproximation.applyOnTheRight(i, j, rotation);
                 }
-          }
-      }
+            }
     }
     // Recalculate divergence for debugging purposes and to check if convergence
     // has been reached
+    //traversalOrder.clear(); otherwise convergence rate is slower
     Scalar divergence = (currentApproximation - initial).norm();
-
     if (m_divOstream) {
-      *m_divOstream << "Divergence: " << std::to_string(divergence) << "\n";
+       *m_divOstream << "Divergence: " << std::to_string(divergence) << "\n";
     }
 
     // Establish if convergence has been reached
